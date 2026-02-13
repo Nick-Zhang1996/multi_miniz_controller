cca7a57f503a0e1bf9cd5e20f7f68498c6ff12e7
Nick Zhang <nickzhang@gatech.edu>
2026-02-13 16:33:22 -0500

    compilable

--- Multiprotocol/Kyosho_a7105.ino
+++ Multiprotocol/Kyosho_a7105.ino
@@ -13,16 +13,12 @@
  along with Multiprotocol.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Pins.h"
 #include "iface_a7105.h"
 
-// #define KYOSHO_FORCE_ID_FHSS
-// #define KYOSHO_FORCE_ID_HYPE
-// #define KYOSHO_FORCE_ID_SYNCRO
-
-// Kyosho constants & variables
+#define KYOSHO_FORCE_ID_FHSS
 #define KYOSHO_BIND_COUNT 2500
 
-#ifndef MULTI_AIR
 static void __attribute__((unused)) KYOSHO_send_packet() {
   // ID
   packet[1] = rx_tx_addr[0];
@@ -80,58 +76,7 @@ static void __attribute__((unused)) KYOSHO_send_packet() {
          0xF0); // last byte is ending with F on the dumps so let's see
     hopping_frequency_no &= 0x1F;
   }
-#if 0
-		debug("ch=%02X P=",rf_ch_num);
-		for(uint8_t i=0; i<37; i++)
-			debug("%02X ", packet[i]);
-		debugln("");
-#endif
   A7105_WriteData(37, rf_ch_num);
-}
-#endif // MULTI_AIR
-
-static void __attribute__((unused)) KYOSHO_hype_send_packet() {
-  if (IS_BIND_IN_PROGRESS) {
-    if (packet_sent == 0) { // build the packet and send it
-      packet[0] = rx_tx_addr[1];
-      packet[1] = rx_tx_addr[3];
-      // RF table
-      for (uint8_t i = 0; i < 15; i++)
-        packet[i + 2] = hopping_frequency[i];
-      A7105_WriteData(17, 0x01);
-      packet_sent++;
-      packet_period = 1421;
-#if 0
-				debug("ch=01 P=");
-				for(uint8_t i=0; i<17; i++)
-					debug("%02X ", packet[i]);
-				debugln("");
-#endif
-    } else
-      A7105_Strobe(A7105_TX); // only send
-  } else {
-    // original TX is only refreshing the packet every 20ms and keep repeating
-    // the same packet in between (STROBE_TX) build packet=6 channels with order
-    // AETR
-    for (uint8_t i = 0; i < 6; i++)
-      packet[i] = convert_channel_8b(CH_AETR[i]);
-    // set RF channel
-    rf_ch_num = hopping_frequency[hopping_frequency_no];
-    hopping_frequency_no++;
-    if (hopping_frequency_no > 14)
-      hopping_frequency_no = 0;
-    // send it
-    A7105_WriteData(6, rf_ch_num);
-    packet_period =
-        931; // packet period fluctuates a lot on the original TX from one
-             // packet to the other but stable if looking over a period of 40ms
-#if 0
-			debug("ch=%02X P=",rf_ch_num);
-			for(uint8_t i=0; i<6; i++)
-				debug("%02X ", packet[i]);
-			debugln("");
-#endif
-  }
 }
 
 uint16_t KYOSHO_callback() {
@@ -150,18 +95,8 @@ uint16_t KYOSHO_callback() {
   } else {
     if (hopping_frequency_no == 0)
       A7105_SetPower();
-#ifdef MULTI_SYNC
-    telemetry_set_input_sync(packet_period);
-#endif
   }
-  if (sub_protocol == KYOSHO_HYPE)
-    KYOSHO_hype_send_packet();
-  else // FHSS && SYNCRO
-#ifndef MULTI_AIR
-    KYOSHO_send_packet();
-#else
-    SUB_PROTO_INVALID;
-#endif
+  KYOSHO_send_packet();
   return packet_period;
 }
 
@@ -169,7 +104,7 @@ void KYOSHO_init() {
   A7105_Init();
 
   // compute channels from ID
-  calc_fh_channels(sub_protocol == KYOSHO_HYPE ? 15 : 32);
+  calc_fh_channels(32);
   hopping_frequency_no = 0;
 
 #ifdef KYOSHO_FORCE_ID_FHSS
@@ -181,33 +116,6 @@ void KYOSHO_init() {
            32);
   }
 #endif
-#ifdef KYOSHO_FORCE_ID_SYNCRO
-  if (sub_protocol == KYOSHO_FHSS) {
-    memcpy(rx_tx_addr, "\x00\xC2\x24\x00", 4);
-    memcpy(hopping_frequency,
-           "\x73\x12\x7D\x88\x63\x4A\x8D\x60\x57\x16\x5D\x8B\x25\x53\x6E\x3C"
-           "\x41\x70\x20\x83\x2A\x19\x94\x2F\x91\x4C\x47\x36\x78\x10\x5A\x31",
-           32);
-  }
-#endif
-  if (sub_protocol == KYOSHO_HYPE) {
-    MProtocol_id &= 0x00FF00FF;
-    rx_tx_addr[0] = 0xAF - (rx_tx_addr[1] & 0x0F);
-    rx_tx_addr[2] = 0xFF - rx_tx_addr[3];
-    MProtocol_id |= (rx_tx_addr[0] << 24) + (rx_tx_addr[2] << 8);
-#ifdef KYOSHO_FORCE_ID_HYPE
-    MProtocol_id = 0xAF90738C;
-    set_rx_tx_addr(MProtocol_id);
-    memcpy(hopping_frequency,
-           "\x27\x1B\x63\x75\x03\x39\x57\x69\x87\x0F\x7B\x3F\x33\x51\x6F", 15);
-#endif
-    if (IS_BIND_IN_PROGRESS)
-      A7105_WriteID(0xAF00FF00);
-    else {
-      A7105_WriteID(MProtocol_id);
-      A7105_WriteReg(A7105_03_FIFOI, 0x05);
-    }
-  }
 
   if (IS_BIND_IN_PROGRESS)
     bind_counter = KYOSHO_BIND_COUNT;
--- Multiprotocol/Multiprotocol.ino
+++ Multiprotocol/Multiprotocol.ino
@@ -265,7 +265,7 @@ static uint32_t random_id(uint16_t address, uint8_t create_new) {
   // Generate a random ID
   (void)address;
   (void)create_new;
-  return FORCE_GLOBAL_ID;
+  return 0x12345678; // Forced global ID, arbitrary
 }
 
 // Generate frequency hopping sequence in the range [02..77]
@@ -340,7 +340,6 @@ static void __attribute__((unused)) crc8_update(uint8_t byte) {
 static void protocol_init() {
   if (IS_WAIT_BIND_off) {
     remote_callback = 0;       // No protocol
-    LED_off;                   // Led off during protocol init
     crc16_polynomial = 0x1021; // Default CRC crc16_polynomial
     crc8_polynomial = 0x31;    // Default CRC crc8_polynomial
     prev_option = option;
@@ -433,34 +432,4 @@ static void random_init(void) {
                  // (WDCE) flag, which is  needed to set the prescaler
   WDTCSR = _BV(WDIE); // Watchdog interrupt enable (WDIE)
   sei();              // Turn interupts on
-}
-
-static uint32_t random_value(void) {
-  while (!gWDT_entropy)
-    ;
-  return gWDT_entropy;
-}
-
-// Random interrupt service routine called every time the WDT interrupt is
-// triggered. It is only enabled at startup to generate a seed.
-ISR(WDT_vect) {
-  static uint8_t gWDT_buffer_position = 0;
-#define gWDT_buffer_SIZE 32
-  static uint8_t gWDT_buffer[gWDT_buffer_SIZE];
-  gWDT_buffer[gWDT_buffer_position] =
-      TCNT1L;             // Record the Timer 1 low byte (only one needed)
-  gWDT_buffer_position++; // every time the WDT interrupt is triggered
-  if (gWDT_buffer_position >= gWDT_buffer_SIZE) {
-    // The following code is an implementation of Jenkin's one at a time hash
-    for (uint8_t gWDT_loop_counter = 0; gWDT_loop_counter < gWDT_buffer_SIZE;
-         ++gWDT_loop_counter) {
-      gWDT_entropy += gWDT_buffer[gWDT_loop_counter];
-      gWDT_entropy += (gWDT_entropy << 10);
-      gWDT_entropy ^= (gWDT_entropy >> 6);
-    }
-    gWDT_entropy += (gWDT_entropy << 3);
-    gWDT_entropy ^= (gWDT_entropy >> 11);
-    gWDT_entropy += (gWDT_entropy << 15);
-    WDTCSR = 0; // Disable Watchdog interrupt
-  }
 }
